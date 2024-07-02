a580f2a24bc45ed5cfde395abc17c49a6b5bcd6d
Johan Svensson <johan.svensson@pts.se>
2024-07-02 16:14:19 +0200

    minor fixes

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,4 @@
-import { sql } from '@vercel/postgres'
+//import { sql } from '@vercel/postgres'
 import { doSqlQuery } from './database'
 
 import {
@@ -24,6 +24,7 @@ export async function fetchRevenue() {
         await new Promise(resolve => setTimeout(resolve, 3000))
 
         const revenue = await doSqlQuery('SELECT * FROM dbo.revenue')
+        console.log('Revenue data received')
         return revenue.first
     } catch (error) {
         console.error('Database Error:', error)
@@ -66,7 +67,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`)
 
-        console.log(invoiceCountPromise)
+        //console.log(invoiceCountPromise)
 
         const data = [invoiceCountPromise, customerCountPromise, invoiceStatusPromise]
 
@@ -221,45 +222,45 @@ export async function fetchCustomers() {
     }
 }
 
-export async function fetchFilteredCustomers(query: string) {
-    try {
-        const data = await sql<CustomersTableType>`
-		SELECT
-		  customers.id,
-		  customers.name,
-		  customers.email,
-		  customers.image_url,
-		  COUNT(invoices.id) AS total_invoices,
-		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
-		FROM customers
-		LEFT JOIN invoices ON customers.id = invoices.customer_id
-		WHERE
-		  customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`}
-		GROUP BY customers.id, customers.name, customers.email, customers.image_url
-		ORDER BY customers.name ASC
-	  `
+// export async function fetchFilteredCustomers(query: string) {
+//     try {
+//         const data = await sql<CustomersTableType>`
+// 		SELECT
+// 		  customers.id,
+// 		  customers.name,
+// 		  customers.email,
+// 		  customers.image_url,
+// 		  COUNT(invoices.id) AS total_invoices,
+// 		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+// 		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+// 		FROM customers
+// 		LEFT JOIN invoices ON customers.id = invoices.customer_id
+// 		WHERE
+// 		  customers.name ILIKE ${`%${query}%`} OR
+//         customers.email ILIKE ${`%${query}%`}
+// 		GROUP BY customers.id, customers.name, customers.email, customers.image_url
+// 		ORDER BY customers.name ASC
+// 	  `
 
-        const customers = data.rows.map(customer => ({
-            ...customer,
-            total_pending: formatCurrency(customer.total_pending),
-            total_paid: formatCurrency(customer.total_paid),
-        }))
+//         const customers = data.rows.map(customer => ({
+//             ...customer,
+//             total_pending: formatCurrency(customer.total_pending),
+//             total_paid: formatCurrency(customer.total_paid),
+//         }))
 
-        return customers
-    } catch (err) {
-        console.error('Database Error:', err)
-        throw new Error('Failed to fetch customer table.')
-    }
-}
+//         return customers
+//     } catch (err) {
+//         console.error('Database Error:', err)
+//         throw new Error('Failed to fetch customer table.')
+//     }
+// }
 
-export async function getUser(email: string) {
-    try {
-        const user = await sql`SELECT * FROM users WHERE email=${email}`
-        return user.rows[0] as User
-    } catch (error) {
-        console.error('Failed to fetch user:', error)
-        throw new Error('Failed to fetch user.')
-    }
-}
+// export async function getUser(email: string) {
+//     try {
+//         const user = await sql`SELECT * FROM users WHERE email=${email}`
+//         return user.rows[0] as User
+//     } catch (error) {
+//         console.error('Failed to fetch user:', error)
+//         throw new Error('Failed to fetch user.')
+//     }
+// }
--- app/lib/database.ts
+++ app/lib/database.ts
@@ -3,16 +3,17 @@ import dbConnect from '../../utils/dbConnect'
 
 export async function getConnection(): Promise<ConnectionPromises> {
     const sql = dbConnect()
-    console.log(`[dashboard] opening connection`)
+    console.log(`[getConnection] opening connection`)
     const con: Connection = await sql.driver.promises.open(sql.connStr)
     return con.promises
 }
 export async function doSqlQuery(sqlQuery: string) {
     const promises: ConnectionPromises = await getConnection()
-    console.log(`[dashboard] executing query: ${sqlQuery}`)
+    console.log(`[getConnection] executing query: ${sqlQuery}`)
     const data = await promises.query(sqlQuery)
-    console.log(`[dashboard] query executed`)
+    console.log(`[getConnection] query executed`)
     await promises.close()
+    console.log(`[getConnection] connection closed`)
 
     return data
 }
